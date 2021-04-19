c612236ee0e38493c2b9c71b879f3680e1db4983
eperl <eperl@umich.edu>
2021-04-19 12:22:06 -0400

    created basic skeleton

---
+++ final_project.py
@@ -0,0 +1,25 @@
+#By Chris Hudson and Ethan Perlmutter
+
+import numpy as np 
+import sqlite3 as sq
+
+
+def getScores():
+    pass
+
+
+def makeTable():
+    pass
+
+def graph1():
+    pass
+
+
+def graph2():
+    pass
+
+
+def main():
+    pass
+
+main()
