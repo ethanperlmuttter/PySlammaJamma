262f85626be6be8fb7183884d79f44f28def1097
eperl <eperl@umich.edu>
2021-04-27 16:02:39 -0400

    created output file

---
+++ NBA_homecourt.py
@@ -0,0 +1,40 @@
+import sqlite3
+import json
+import csv
+import os
+
+
+def output_file():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+"NBA_SCORES")
+    cur = conn.cursor()
+    csvfile = '2020_home_ppg.csv'
+    with open(csvfile, 'w') as of:
+        of = csv.writer(of, delimiter = ",")
+        of.writerow(["Team", "Home_PPG"])
+
+        cur.execute("SELECT * FROM Team_keys JOIN Scores_2020 ON Team_keys.team_id = Scores_2020.Home_id")
+
+        teams = cur.fetchall()
+        
+        d = {}
+        for tup in teams:
+            sum_ = d.get(tup[1], (0,0))[0] + tup[6]
+            n = d.get(tup[1], (0,0))[1] + 1
+
+            d[tup[1]] = (sum_, n)
+        
+        for i in d:
+            of.writerow([i, (d[i][0]/d[i][1]) ]) 
+
+
+
+
+
+output_file()
+
+    
+
+       
+        
+
